7ff3f1b3f3c36087165e0ecb93ee41e4b85ead53
kovrigo <aekovrigo@gmail.com>
2021-03-10 13:23:34 +0400

    Dev

--- rollup.config.es6.js
+++ rollup.config.es6.js
@@ -1,6 +1,10 @@
 import config from './rollup.config';
 
 export default Object.assign(config, {
-    entry: 'src/index.es6.js',
-    format: 'es6'
+    //entry: 'src/index.es6.js',
+    //format: 'es6'
+    input: 'src/index.es6.js',
+	output: {
+		format: 'es6',
+	},    
 });
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-    entry: 'src/index.js',
-    format: 'umd',
-    moduleName: 'Eloquent',
-    plugins: [
-        babel({
-            presets: ['es2015-rollup'],
-            babelrc: false
-        })
-    ]
+
+	input: 'src/index.js',
+	output: {
+		format: 'umd',
+		name: 'Eloquent',
+	},
+	plugins: [
+		babel({
+			presets: ['@babel/env'],
+			babelrc: false
+		})
+	]
+    //entry: 'src/index.js',
+    //format: 'umd',
+    //moduleName: 'Eloquent',
 };
